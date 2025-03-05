6b6d5ee7fc468342726eeab36937d1aec9c4f4a2
Ray Ozzie <ray@ozzie.net>
2025-03-04 23:34:43 -0500

    m

--- go/ear_protection.go
+++ go/ear_protection.go
@@ -4,19 +4,30 @@ package codec2
 // It finds the maximum sample in the buffer and, if above a threshold,
 // scales the entire buffer down.
 func earProtection(inOut []float64, n int) {
+
+	// find maximum sample in frame
+
 	maxSample := 0.0
 	for i := 0; i < n; i++ {
-		if abs(inOut[i]) > maxSample {
-			maxSample = abs(inOut[i])
+		if inOut[i] > maxSample {
+			maxSample = inOut[i]
 		}
 	}
-	threshold := 30000.0
-	if maxSample > threshold {
-		gain := 1.0 / (maxSample * maxSample)
-		for i := 0; i < n; i++ {
+
+	// determine how far above set point
+
+	over := maxSample / 30000.0
+
+	// If we are x B over set point we reduce level by 2x dB, this
+	// attenuates major excursions in amplitude (likely to be caused
+	// by bit errors) more than smaller ones
+	if over > 1.0 {
+		gain := 1.0 / (over * over);
+		for i := 0; i<n; i++ {
 			inOut[i] *= gain
 		}
 	}
+
 }
 
 func abs(x float64) float64 {
